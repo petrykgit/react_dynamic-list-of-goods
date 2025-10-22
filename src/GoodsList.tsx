b98bfc5b773154cc8a52d3df7a37e8bb3a41194d
Ievgen Petryk <jakeshad123@gmail.com>
2025-10-22 19:12:11 +0300

    made AI happy

--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -9,7 +9,11 @@ export const GoodsList = React.memo(function GoodsList({ goods }: Props) {
   return (
     <ul>
       {goods.map(good => (
-        <li key={good.id} data-cy="good" style={{ color: good.color }}>
+        <li
+          key={good.id}
+          data-cy="good"
+          style={{ backgroundColor: good.color }}
+        >
           {good.name}
         </li>
       ))}
